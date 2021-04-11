17ceebb9e40939f5041cd735741fee05c60e1226
Malgorzata Wladyka <mwladyka126@gmail.com>
2021-04-11 14:46:58 +0200

    add dep to massage

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -53,7 +53,7 @@ router.put("/departments/:id", async (req, res) => {
         { _id: req.params.id },
         { $set: { name: name } }
       );
-      res.json({ message: "OK" });
+      res.json({ message: "OK", changedDep: dep });
     } else res.status(404).json({ message: "Not found..." });
   } catch (err) {
     res.status(500).json({ message: err });
@@ -65,7 +65,7 @@ router.delete("/departments/:id", async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if (dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: "OK" });
+      res.json({ message: "OK", deletedDep: dep });
     } else res.status(404).json({ message: "Not found..." });
   } catch (err) {
     res.status(500).json({ message: err });
